80d0f4136ae4dfb0dd9b0404aa385977160a4e2f
Igor Sutton <igorsutton@me.com>
2012-03-15 11:14:11 +0100

    Added band filter.

---
+++ IARL/IARLBandFilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  IARLBandFilterViewController.h
+//  IARL
+//
+//  Created by Igor Sutton on 3/15/12.
+//  Copyright (c) 2012 igorsutton.com. All rights reserved.
+//
+
+@interface IARLBandFilterViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *bands;
+
+@end
---
+++ IARL/IARLBandFilterViewController.m
@@ -0,0 +1,95 @@
+//
+//  IARLBandFilterViewController.m
+//  IARL
+//
+//  Created by Igor Sutton on 3/15/12.
+//  Copyright (c) 2012 igorsutton.com. All rights reserved.
+//
+
+#import "IARLBandFilterViewController.h"
+
+@interface IARLBandFilterViewController ()
+
+@end
+
+@implementation IARLBandFilterViewController
+
+@synthesize bands = _bands;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    _bands = [[NSArray alloc] initWithObjects:@"2m", @"1m", nil];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return [self.bands count];
+            break;
+            
+        default:
+            break;
+    }
+    return 0;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    switch (section) {
+        case 0:
+            return @"Bands";
+            break;
+            
+        default:
+            break;
+    }
+    
+    return @"Foo";
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    switch (indexPath.section) {
+        case 0:
+            cell.textLabel.text = [self.bands objectAtIndex:indexPath.row];
+            break;
+            
+        default:
+            break;
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    if (indexPath.section == 0) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
