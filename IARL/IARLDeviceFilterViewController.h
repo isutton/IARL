23530fca330b6b97b5e4ccddc89dc40ce1ac390f
Igor Sutton <igorsutton@me.com>
2012-03-15 11:40:24 +0100

    Added device filter controller.

---
+++ IARL/IARLDeviceFilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  IARLDeviceFilterViewController.h
+//  IARL
+//
+//  Created by Igor Sutton on 3/15/12.
+//  Copyright (c) 2012 igorsutton.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IARLDeviceFilterViewController : UITableViewController
+
+@end
